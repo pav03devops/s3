dbfe5657b7c8267df339e573892b3823fe861776
Praveen <pav18nov@gmail.com>
2023-12-19 13:01:44 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-for-praveen"
+  
+  tags = {
+    Name        = "My-bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+    name            = "terraform-lock"
+    billing_mode    = "PAY_PER_REQUEST"
+    hash_key        = "LockID"
+
+    attribute {
+      name = "LockID"
+      type = "S"
+    }
+}
